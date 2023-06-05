26f7e7b0e95932873c525ffe01447a1978fab484
Madhavi Sapkota <madhavi.sapkota@gmail.com>
2023-06-05 17:04:58 -0500

    setting up react

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World!</h1>
+      {/* Header*/}
+      <Header />
+      {/* Appbody*/}
+      {/* sidebar*/}
+      {/* feed*/}
+      {/* widgets*/}
+    </div>
+  );
+}
+
+export default App;
