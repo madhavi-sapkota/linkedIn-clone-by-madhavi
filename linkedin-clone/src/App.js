9e9400c41baa88b192e911c1d16021d8dfbdcc80
Madhavi Sapkota <madhavi.sapkota@gmail.com>
2023-06-06 09:42:24 -0500

    Header in progress

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="App">
-      <h1> Hello World!</h1>
       {/* Header*/}
       <Header />
       {/* Appbody*/}
--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -1,10 +1,34 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 function Header() {
   return (
     <div className="header">
-      <h1>This is the header</h1>
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+      </div>
     </div>
   );
 }
