9780f42372d00cf61cc1d1d18c9d137e242b6878
Madhavi Sapkota <madhavi.sapkota@gmail.com>
2023-06-08 13:01:29 -0500

    sidebar completed

--- linkedin-clone/src/Sidebar.js
+++ linkedin-clone/src/Sidebar.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar } from "@mui/material";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__has">#</span>
+      <p>{topic}</p>
+    </div>
+  );
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img
+          src="https://images.pexels.com/photos/956999/milky-way-starry-sky-night-sky-star-956999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Madhavi Sapkota</h2>
+        <h4>madhavisapkota1@gmail.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,296</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,287</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactJs")}
+        {recentItem("programming")}
+        {recentItem("software engineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
       </div>
     </div>
   );
